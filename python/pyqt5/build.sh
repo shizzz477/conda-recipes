9c2bd9e78bdbe1c06fb9b741f80f4f47e8d9ff90
shizzz477 <marcgurreri@gmail.com>
2017-07-15 15:20:08 +0900

    update

--- python/pyqt5/build.sh
+++ python/pyqt5/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/lib/nvidia-cuda-toolkit/bin
+
 if [ `uname` == Darwin ]; then
     export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
     MAKE_JOBS=$(sysctl -n hw.ncpu)
@@ -10,9 +12,19 @@ if [ `uname` == Linux ]; then
 fi
 
 $PYTHON configure.py \
-        --verbose \
-        --confirm-license \
+        -verbose \
+        -confirm-license \
         --assume-shared \
+        -opengl desktop \
+        -gstreamer \
+        -no-compile-examples \
+        -qt-pcre \
+        -qt-xcb \
+        -qt-freetype \
+        -qt-libjpeg \
+        -qt-libpng \
+        -qt-zlib \
+        -opensource /
         -q $PREFIX/bin/qmake-qt5
 
 make -j $MAKE_JOBS
